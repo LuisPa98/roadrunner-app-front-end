120e235256bceeee9ebe6028c2b8b70e6c332b5c
Ambre Tate <ambretate@gmail.com>
2024-04-10 10:50:38 -0400

    Setup of Screens, Components, and Services files

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./navBar.css";
+
+function NavBar() {
+  return <div>NavBar</div>;
+}
+
+export default NavBar;
---
+++ src/Screens/Follows/Follows.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./follows.css";
+
+function Follows() {
+  return <div>Follows</div>;
+}
+
+export default Follows;
