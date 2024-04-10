ee3594c0923ac851562ad01b6256a2a16f840069
Ambre Tate <ambretate@gmail.com>
2024-04-10 11:39:22 -0400

    initial setup of Layout component to render Header and NavBar

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import Header from "../Header/Header.jsx";
+import NavBar from "../NavBar/NavBar.jsx";
 import "./layout.css";
 
-function Layout() {
-  return <div>Layout</div>;
+function Layout(props) {
+  return (
+    <div>
+      <div id="layoutBodyContainer">
+        <Header />
+        <div id="layoutBody">{props.children}</div>
+      </div>
+      <NavBar />
+    </div>
+  );
 }
 
 export default Layout;
